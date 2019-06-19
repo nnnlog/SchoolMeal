9802770772b416a8c3358961d894d93929117664
nnnlog <sorisem4106@naver.com>
2019-06-20 00:40:13 +0900

    license

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,10 @@
-const SchoolMeal = require("schoolmeal");
+const SchoolMeal = require("../src/SchoolMeal");
 process.env.TZ = 'Asia/Seoul';
 
 let meal = new SchoolMeal("BUSAN", 4, "C100000394");
-meal.getMonthlyMeal(new Date(2019, 3, 21)).then(meals => {
-    console.log(meals[15]);
+meal.getMonthlyMeal(new Date(2019, 6, 19)).then(w_meal => {
+    console.log(w_meal[19].getDinner()); //석식: 밥버거
 });
-meal.getWeeklyMeal(new Date(2019, 3, 21)).then(meals => {
-    console.log(Object.values(meals)[2]);
+meal.getWeeklyMeal(new Date(2019, 6, 19)).then(meals => {
+    console.log(Object.values(meals)[4]); //석식: 밥버거
 });
